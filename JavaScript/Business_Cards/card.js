1b5bdcd23bf6952ce1c1a6b868e24aa33d0fb20d
Mukto Akash <73410983+muktoakash@users.noreply.github.com>
2024-02-21 21:03:00 +0530

    Create printCard function

---
+++ JavaScript/Business_Cards/card.js
@@ -0,0 +1,8 @@
+// define the functions
+function printCard() {
+    var name_line = "<strong>Name: </strong>" + this.name + "<br>\n";
+    var email_line = "<strong>Email: </strong>" + this.email + "<br>\n";
+    var address_line = "<strong>Address: </strong>" + this.address + "<br>\n";
+    var phone_line = "<strong>Phone: </strong>" + this.phone + "<hr>\n";
+    document.write(name_line, email_line, address_line, phone_line);
+}
