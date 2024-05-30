83aeca9db6da196c8e4d6c42524cb4c8f0c516bd
Mukto Akash <muktoakash@gmail.com>
2024-05-30 21:45:00 +0530

    Complete userLocation.html

--- HTML/GeoLocation/userLocation.js
+++ HTML/GeoLocation/userLocation.js
@@ -1,26 +1,32 @@
 // Initialize and display the map
 function initMap() {
-    // Create a new map instance
-    var map = L.map("map").setView(location, 12);
-
-    // Get the user's location
+// Get the user's location
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(
             function (position) {
-                // Retriece the users latitude and longitude
-                var userlocation = {
-                    lat: position.coords.latitude,
-                    lng: position.coords.longitude,
-                };
+              // Retriece the users latitude and longitude
+              var userLocation = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude,
+              };
 
-                // Center the map on the user's location
-                map.setCenter(userlocation);
+              // Create a new map instance
+              var map = L.map("map").setView(userLocation, 12);
 
-                // Add a marker to the user's location
-                L.marker(userLocation)
-                    .addTo(map)
-                    .bindPopup("Your Location")
-                    .openPopup();
+              // Add a marker to the user's location
+              L.marker(userLocation)
+                .addTo(map)
+                .bindPopup("Your Location")
+                .openPopup();
+
+              // Add a tile layer (OpenStreetMap)
+              L.tileLayer(
+                "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+                {
+                  attribution:
+                    'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+                }
+              ).addTo(map);
             },
             function (error) {
                 console.error("Error getting user location:", error);
@@ -29,10 +35,4 @@ function initMap() {
     } else {
         console.error("Geolocation is not supported by this browser.");
     }
-
-    // Add a tile layer (OpenStreetMap)
-    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-        attribution:
-        'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-    }).addTo(map);
 }
