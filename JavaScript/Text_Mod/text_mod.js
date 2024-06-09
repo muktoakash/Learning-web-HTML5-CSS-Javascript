8992d361d3076c393ef3e3ffefb938ac061b68f5
Mukto Akash <muktoakash@gmail.com>
2024-06-08 23:14:29 -0400

    Add styling to Text_Mod

--- JavaScript/Text_Mod/text_mod.js
+++ JavaScript/Text_Mod/text_mod.js
@@ -4,3 +4,11 @@ function changeTitle() {
     var heading1 = document.getElementById("heading1");
     heading1.firstChild.nodeValue = newTitle;
 }
+
+function addText() {
+    if (!document.getElementById) return;
+    var sentence = document.changeForm2.sentence.value;
+    var node = document.createTextNode(" " + sentence);
+    document.getElementById("paragraph1").appendChild(node);
+    document.changeForm2.sentence.value = "";
+}
