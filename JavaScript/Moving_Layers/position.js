c2d314e902dfabe66e52a74dd108af0e5cd5fe5d
Mukto Akash <muktoakash@gmail.com>
2024-06-08 22:53:29 -0400

    Add pos functionality to Moving_Layers

---
+++ JavaScript/Moving_Layers/position.js
@@ -0,0 +1,10 @@
+var x = 100, y = 150;
+
+function pos(dx, dy) {
+    if (!document.getElementById) return;
+    x += 30 * dx;
+    y += 30 * dy;
+    obj = document.getElementById("square");
+    obj.style.top = y + "px";
+    obj.style.left = x + "px";
+}
