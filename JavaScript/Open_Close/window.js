c4678a584fb4a66c4b21d47a433df4b12cee3c31
Mukto Akash <muktoakash@gmail.com>
2024-06-19 22:08:03 -0400

    Complete Open_Close

--- JavaScript/Open_Close/window.js
+++ JavaScript/Open_Close/window.js
@@ -5,3 +5,24 @@ document.getElementById("openNew").addEventListener("click", function ({
 document.getElementById("closeNew").addEventListener("click", function ({
     newWin.close();
 }));
+
+function doIt() {
+    if ((document.changeForm.w.value) &&
+        (document.changeForm.h.value)) {
+        newWin.resizeTo(document.changeForm.w.value,
+            document.changeForm.h.value
+        );
+    }
+    if ((document.changeForm.x.value) &&
+        (document.changeForm.y.value)) {
+        newWin.moveTo(document.changeForm.x.value,
+            document.changeForm.y.value
+        );
+    }
+}
+
+window.addEventListener("load", function () {
+    newWin = this.window.open('', "NewWin", 'width=200,height=100');
+})
+
+document.getElementById("doIt").addEventListener("click", doIt);
