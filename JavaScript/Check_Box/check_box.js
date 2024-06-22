0fc568e4e9b57ce8e6fd74a9c4fbb1172879e1d7
Mukto Akash <muktoakash@gmail.com>
2024-06-22 03:56:42 -0400

    Code Toggle functionality for Check_Box

--- JavaScript/Check_Box/check_box.js
+++ JavaScript/Check_Box/check_box.js
@@ -20,3 +20,26 @@ function graphicBox(box) {
     // display the graphic checkbox
     parentObj.insertBefore(img, obj);
 }
+
+function Toggle(e) {
+    if (!e) var e = window.event;
+
+    // find the image ID
+    img = (e.target) ? e.target : e.srcElement;
+
+    // find the checkbox by removing "img" from the image ID
+    checkid = img.id.substring(3);
+    checkbox = document.getElementById(checkid);
+
+    // click the checkbox
+    checkbox.click();
+
+    // display the right image for the clicked or unclicked state
+    if (checkbox.checked) {
+        file = "./assets/checked.png";
+    } else {
+        file = "/assets/unchecked.png";
+    }
+
+    img.src = file;
+}
