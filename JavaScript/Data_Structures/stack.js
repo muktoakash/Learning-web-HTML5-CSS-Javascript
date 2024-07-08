993de0c7edd919580d2a669d60091ce1327bff21
Mukto Akash <muktoakash@gmail.com>
2024-07-08 07:40:19 -0400

    Complete functionalities for stack.js

--- JavaScript/Data_Structures/stack.js
+++ JavaScript/Data_Structures/stack.js
@@ -6,5 +6,25 @@ class Stack {
 
     push(value) {
         this.storage[this.size] = value;
+        this.size++;
+    }
+
+    pop() {
+        if (this.size === 0) {
+            return undefined;
+        }
+
+        this.size--;
+        var item = this.storage[this.size];
+        delete this.storage[this.size];
+        return item;
+    }
+
+    length() {
+        return this.size;
+    }
+
+    peek(value = this.size) {
+        return this.storage[value - 1];
     }
 }
