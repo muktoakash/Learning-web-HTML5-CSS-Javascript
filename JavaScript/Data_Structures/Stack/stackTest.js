0a3e2cd0be578f85a5b9d12aa5f6a2cf44fccd67
Mukto Akash <73410983+muktoakash@users.noreply.github.com>
2024-07-09 20:49:28 +0000

    To be reverted

--- JavaScript/Data_Structures/Stack/stackTest.js
+++ JavaScript/Data_Structures/Stack/stackTest.js
@@ -3,5 +3,13 @@
 // with the help of stackTest.html for visualization
 
 // Import
-import { Stack } from "../structures";
+// import { Stack } from "../structures";
 
+// Create a new Stack when button is push
+// Construct a new Stack
+// Stack newStack = new Stack();
+document.getElementById("createStack").addEventListener("click", () => {
+    // txt = document.getElementById("pIntroFormStack").textContent;
+    // txt = "Stack has been initialized. " + txt;
+    document.getElementById("introFormStack").innerText = "Now";
+})
