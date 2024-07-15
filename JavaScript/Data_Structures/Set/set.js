3c85a78290feb872fa69ef72e7e1fc511a9b3ce3
Mukto Akash <muktoakash@gmail.com>
2024-07-14 22:29:48 -0400

    Clean up set.js

--- JavaScript/Data_Structures/Set/set.js
+++ JavaScript/Data_Structures/Set/set.js
@@ -1,13 +1,10 @@
 class SetofStrings{ // Named to avoid conflict with ES6 Set
     constructor() {
-        // Use an array to hold the elements (not efficient for lookup)
-        // May be modify later with a hashtable
         this.collection = {};
         this.size = 0;
     }
 
     hasElement(element) {
-       // return (this.collection.indexOf(element) !== -1);
         if (this.collection[String(element)]) {
             return true;
         }
@@ -15,13 +12,12 @@ class SetofStrings{ // Named to avoid conflict with ES6 Set
     }
 
     getAll() {
-        // return this.collection;
         return Object.keys(this.collection);
     }
 
+
     add(item) {
         if (!this.hasElement(item)) {
-            // this.collection.push(item);
             this.collection[String(item)] = true;
             this.size++;
             return true;
@@ -31,8 +27,6 @@ class SetofStrings{ // Named to avoid conflict with ES6 Set
 
     remove(item) {
         if (this.hasElement(item)) {
-            // let index = this.collection.indexOf(item);
-            // this.collection.splice(index, 1);
             this.collection[String(item)] = false;
             this.size--;
             return true;
@@ -43,6 +37,8 @@ class SetofStrings{ // Named to avoid conflict with ES6 Set
     getSize() { return this.size; }
 
     isEmpty() { return this.size === 0; }
+
+    // Implement set union, intersection, difference, Cartesian Project, Power Set, isSubset
 
 
 }
