04507166548caec8d0ad2b3ff87f7edac16d5e96
Mukto Akash <muktoakash@gmail.com>
2024-09-12 05:38:07 -0400

    Get set input and button to function on setOps.js

--- JavaScript/Data_Structures/Set/setOps.js
+++ JavaScript/Data_Structures/Set/setOps.js
@@ -1,2 +1,13 @@
 // setOps.js
 // Tests the implementation of set operations from set.js
+
+const setA = new SetOfStrings();
+const setB = new SetOfStrings();
+
+document.querySelector("#createSetOps").addEventListener("click", () => {
+    let setAInput = document.getElementById("insertSetA").value;
+    let setAList = setAInput.split(",");
+    setAList = setAList.map(x => x.trim());
+    console.log(setAList);
+}
+)
