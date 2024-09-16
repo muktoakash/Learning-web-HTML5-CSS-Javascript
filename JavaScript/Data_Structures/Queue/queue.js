a1231e0bb3a040d1fcb8e7ab51a3c130cb31b157
Mukto Akash <muktoakash@gmail.com>
2024-09-15 22:55:17 -0400

    Add basic functionalities of queue.js

--- JavaScript/Data_Structures/Queue/queue.js
+++ JavaScript/Data_Structures/Queue/queue.js
@@ -1,2 +1,32 @@
 // ./queue.js
 // Queue and Priority Queue data structure
+
+class Queue{
+
+    constructor() {
+        this.collection = [];
+        this.size = 0;
+    }
+
+    enqueue = (element) => {
+        this.collection.push(element);
+        this.size++;
+    }
+
+    dequeue = (element) => {
+        this.size--;
+        return this.collection.shift();
+    }
+
+    front = () => {
+        return this.collection[0];
+    }
+
+    getSize = () => {
+        return this.size;
+    }
+
+    isEmpty = () => {
+        return (this.getSize() === 0);
+    }
+}
