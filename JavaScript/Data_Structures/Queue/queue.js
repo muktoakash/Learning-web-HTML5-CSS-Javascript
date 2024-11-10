0bd3ea97b6c9ba52fac53831b01031c0b17dec8c
Mukto Akash <muktoakash@gmail.com>
2024-11-10 15:45:40 -0500

    Add dequeue function for PQ in queue.js

--- JavaScript/Data_Structures/Queue/queue.js
+++ JavaScript/Data_Structures/Queue/queue.js
@@ -86,16 +86,27 @@ class PriorityQueue(){
     enque = (element) => {
         const item_key = element[1];
         const item_value = element[0]
-        if (sorted_priorities.indexOf(item_key) === -1) {
+        if (this.sorted_priorities.indexOf(item_key) === -1) {
             this.collection[item_key] = [item_value];
+            this.prioritiesAddAndSort(item_key);
         } else {
             this.collection[item_key].push(item_value);
         }
         this.size += 1;
-        this.sorted_priorities
     }
 
     dequeue = () => {
-
+        if (!this.isPQEmpty()) {
+            const item_key = this.sorted_priorities[0];
+            // if more than one item at that priority, return the latest.
+            const item_value = this.collection[item_key].pop();
+            if (this.collection[item_key].length === 0) {
+                // No more items left at this priority
+                this.sorted_priorities.shift();
+            }
+            return item_value;
+        }
+        // trying to dequeue from an empty PQ results in undefined.
+        return undefined;
     }
 }
