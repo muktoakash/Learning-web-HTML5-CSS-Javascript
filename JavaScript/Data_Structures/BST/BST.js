644288f2ca102a8f4ff497dafae4385064180794
Mukto Akash <muktoakash@gmail.com>
2024-12-13 21:06:33 -0500

    Establish node and BST classes in BST.js

--- JavaScript/Data_Structures/BST/BST.js
+++ JavaScript/Data_Structures/BST/BST.js
@@ -1,1 +1,20 @@
 // BST/BST.js
+
+class Node {
+    // A node is
+    //      data
+    //      /   \
+    //  left    right
+    constructor(data, left = null, right = null) {
+        var this.data = data;
+        var this.left = left;
+        var this.right = right;
+    }
+}
+
+class BST {
+    // A BST is a root node with nodes as children
+    constructor(root = null) {
+        var this.root = root;
+    }
+}
