10d8a23fdc53d7068a15b611c39cee42a4eff92a
Mukto Akash <muktoakash@gmail.com>
2024-12-14 15:40:23 -0500

    Set up skeleton for TVMaze API

---
+++ API/TVMaze/search.js
@@ -0,0 +1,16 @@
+// TVMaze/search.html
+// Async/await functionalities to interact with the TVMaze API
+
+// Select form elements
+const btn = document.getElementById("buttonSearch");
+const input = document.getElementById("inputSearch");
+
+// Event handler for button
+btn.addEventListener('click', async () => {
+    const searchTerm = input.value;
+
+})
+
+// async function to handle API
+
+// display modifier function
