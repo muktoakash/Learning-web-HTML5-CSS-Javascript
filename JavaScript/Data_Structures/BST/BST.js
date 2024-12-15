3176211d3e22e28b4ef0328c5af607d9ee77a36f
Mukto Akash <muktoakash@gmail.com>
2024-12-15 05:11:59 -0500

    Create add() function in BST.js

--- JavaScript/Data_Structures/BST/BST.js
+++ JavaScript/Data_Structures/BST/BST.js
@@ -17,4 +17,36 @@ class BST {
     constructor(root = null) {
         var this.root = root;
     }
+    add(data) {
+        const node = this.root;
+        if (node == null) {
+            this.root = new Node(data);
+            return;
+        }
+        else {
+            const searchTree = function (node) {
+                if (data < node.data) {
+                    if (node.left === null) {
+                        node.left = new Node(data);
+                        return;
+                    } else if (node.left !== null) {
+                        return searchTree(node.left);
+                    }
+                }
+                else if (data > node.data) {
+                    if (node.right === null) {
+                        node.right = new Node(data);
+                        return;
+                    }
+                    else if (node.right !== null) {
+                        return searchTree(node.right);
+                    }
+                }
+                else {
+                    return null;
+                }
+            };
+            return searchTree(node);
+        }
+    }
 }
