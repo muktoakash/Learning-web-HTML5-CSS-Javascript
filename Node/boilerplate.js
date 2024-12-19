382631e91c47d1d0ff2044ec2d2027d46124faf8
Mukto Akash <muktoakash@gmail.com>
2024-12-18 23:14:21 -0500

    Update documentation of  boilerplate.js

--- Node/boilerplate.js
+++ Node/boilerplate.js
@@ -1,1 +1,15 @@
 // Node/boilerplate.js
+/* Boilerplate to create files to start a web-project.
+Takes the project name as a command-line argument.
+Creates a folder, and within it html, css, and js files
+with the same name as the argument. */
+
+const projectName = process.argv[2] || 'Project';
+const fs = require('fs');
+
+try {
+
+}
+catch (e) {
+    console.log(e);
+}
