eb3874531cfba736db6e562a2f92563aba147181
Mukto Akash <muktoakash@gmail.com>
2024-12-20 04:08:20 -0500

    Complete lang_guesser

---
+++ Node/lang_guesser/index.js
@@ -0,0 +1,15 @@
+const franc = require('franc');
+const langs = require('langs');
+const colors = require('colors');
+
+const input = process.argv[2];
+const langCode = franc(input);
+
+if (langCode === 'und') {
+    console.log("Sorry, couldn't find the language.".red);
+} else {
+    const language = langs.where("3", langCode);
+
+    console.log(`Our best guess is ${language.name}`.green);
+
+}
