9a5b86004c4e9b8ba491d2509800da018b527384
Mukto Akash <muktoakash@gmail.com>
2024-12-21 10:07:34 -0500

    Create get and post functions in firstapp for Express

--- Node/Express/firstapp/index.js
+++ Node/Express/firstapp/index.js
@@ -1,7 +1,25 @@
 const firstapp = require('express');
 const app = express();
 
-app.use((req, res) = {
+// app.use((req, res) = {
+
+// })
+
+app.get('/', (req, res) => {
+    res.send("This is the homepage");
+
+})
+
+app.post('/', (req, res) => {
+    res.send("This was a homepage post req");
+})
+
+app.get('*', (req, res) => {
+    res.send("This path I do not know");
+})
+
+app.post('*', (req, res) => {
+    res.send("post req sent to unknown path");
 
 })
 
