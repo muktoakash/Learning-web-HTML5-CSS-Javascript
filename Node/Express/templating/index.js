cce0502108f19cf569dd89a6b49a75feacecec05
Mukto Akash <muktoakash@gmail.com>
2024-12-21 23:50:48 -0500

    Run inital template for express

--- Node/Express/templating/index.js
+++ Node/Express/templating/index.js
@@ -1,12 +1,12 @@
-const express = required('express');
+const express = require('express');
 const app = express();
 
-app.set('view engine', ejs)
+app.set('view engine', 'ejs');
 
 app.get("/", (req, res) => {
-
+    res.render('home.ejs');
 })
 
 app.listen(3000, () => {
-    console.log("Listening on Port 3000")
+    console.log("Listening on Port 3000");
 })
