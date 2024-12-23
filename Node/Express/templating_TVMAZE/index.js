97bc0833154f675c76712dc4fb9aa813a545748f
Mukto Akash <muktoakash@gmail.com>
2024-12-22 19:04:35 -0500

    Complete initial boilerplate for index.js in template_TVMAZE

--- Node/Express/templating_TVMAZE/index.js
+++ Node/Express/templating_TVMAZE/index.js
@@ -13,7 +13,7 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'));
 
 app.get('/', (req, res) => {
-    res.send('home');
+    res.render('home.ejs');
 })
 
 app.listen(3000, () => {
