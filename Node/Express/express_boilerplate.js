dd3de5effc343d8759b12a59d935973c0ac3303c
Mukto Akash <muktoakash@gmail.com>
2024-12-22 19:19:20 -0500

    Create version 1 of express_boilerplate

---
+++ Node/Express/express_boilerplate.js
@@ -0,0 +1,19 @@
+/ Node/Express/express_boilerplate.js
+/* Boilerplate to create files to start an npm-express-ejs project.
+Uses common naming conventions
+Creates a folder, and files, as usual.*/
+
+const fs = require('fs');
+
+try {
+    fs.mkdirSync(public);
+    fs.mkdirSync(views);
+    fs.mkdirSync(views/partials);
+    fs.writeFileSync(`index.js`, '')
+    fs.writeFileSync(`views/home.ejs`, '')
+    fs.writeFileSync(`views/partials/head.ejs`, '')
+    fs.writeFileSync(`views/partials/foot.ejs`, '')
+}
+catch (e) {
+    console.log(e);
+}
