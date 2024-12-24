a0eadbf17af06f114498b7f8e8e2a984b36da95e
Mukto Akash <muktoakash@gmail.com>
2024-12-24 16:04:39 -0500

    Include slugify in index.js for template_TVMAZE

--- Node/Express/templating_TVMAZE/index.js
+++ Node/Express/templating_TVMAZE/index.js
@@ -6,6 +6,16 @@ const app = express();
 // const bootstrap = require('bootstrap');
 const path = require('path');
 
+var slugify = require("slugify");
+// slugify('some string', {
+//   replacement: '-',  // replace spaces with replacement character, defaults to `-`
+//   remove: undefined, // remove characters that match regex, defaults to `undefined`
+//   lower: false,      // convert to lower case, defaults to `false`
+//   strict: false,     // strip special characters except replacement, defaults to `false`
+//   locale: 'vi',      // language code of the locale to use
+//   trim: true         // trim leading and trailing replacement chars, defaults to `true`
+// })
+
 app.use(express.static(path.join(__dirname, '/public')));
 
 // const ejs = require('ejs');
