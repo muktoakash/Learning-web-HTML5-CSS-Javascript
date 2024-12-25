208a5198d90193ea7971784ea4620d589dac1135
Mukto Akash <muktoakash@gmail.com>
2024-12-24 19:31:56 -0500

    First functional version of template_TVMAZE

--- Node/Express/templating_TVMAZE/index.js
+++ Node/Express/templating_TVMAZE/index.js
@@ -2,9 +2,14 @@
 
 const express = require('express');
 const app = express();
+// const bodyParser = require("body-parser");
 
 // const bootstrap = require('bootstrap');
 const path = require('path');
+const axios = require('axios');
+// const httpProxy = require("express-http-proxy");
+
+
 
 var slugify = require("slugify");
 // slugify('some string', {
@@ -16,6 +21,7 @@ var slugify = require("slugify");
 //   trim: true         // trim leading and trailing replacement chars, defaults to `true`
 // })
 
+// app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 app.use(express.static(path.join(__dirname, '/public')));
 
 // const ejs = require('ejs');
@@ -27,8 +33,40 @@ app.get('/', (req, res) => {
 })
 
 app.get('/results', (req, res) => {
-    console.log('Request successful!');
-    res.render('results.ejs');
+
+    const searchTerm = req.query.inputSearch;
+
+    // / async function to handle API
+    async function getMoviesData(searchTerm) {
+        try {
+            const config = { params: { q: searchTerm } };
+            resp = await axios.get(`https://api.tvmaze.com/search/shows`, config)
+            return resp.data;
+        }
+        catch (e) {
+            console.log('Trouble getting Movies')
+            return null;
+        }
+    }
+
+    if (searchTerm) {
+        try {
+            result = getMoviesData(searchTerm);
+            result.then((data) => {
+                // console.log(data);
+                res.render('results.ejs', { searchTerm, data });
+            });
+        }
+        catch (e) {
+            console.log(e);
+        }
+    }
+    else {
+        res.render('results.ejs', { searchTerm: '', data: '' });
+    }
+
+
+
 })
 
 app.listen(3000, () => {
