6ec88441cdeaad861fe7bd0102c798130036ce47
Mukto Akash <muktoakash@gmail.com>
2024-12-31 11:26:13 -0500

    Copy over index template to RESTDEMO

---
+++ Node/Express/RESTDemo/index.js
@@ -0,0 +1,21 @@
+// index.js
+
+const express = require("express");
+const app = express();
+
+// const bootstrap = require('bootstrap');
+const path = require("path");
+
+app.use(express.static(path.join(__dirname, "/public")));
+
+// const ejs = require('ejs');
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"));
+
+app.get("/", (req, res) => {
+  res.render("home.ejs");
+});
+
+app.listen(3000, () => {
+  console.log("LISTENING ON PORT 3000");
+});
