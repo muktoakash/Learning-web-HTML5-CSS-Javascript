c104a7059b87f70e30c3774cde5cd05f686fb0fa
Mukto Akash <muktoakash@gmail.com>
2024-12-30 21:29:55 -0500

    Set get route for RESTDemo

--- Node/Express/RESTDemo/index.js
+++ Node/Express/RESTDemo/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 
+app.get('/tacos', (req, res) => {
+    res.send("GET /tacos response");
+})
 app.listen(3000, () = {
-    console.log("ON PORT 3000!")
+    console.log("ON PORT 3000!");
 })
