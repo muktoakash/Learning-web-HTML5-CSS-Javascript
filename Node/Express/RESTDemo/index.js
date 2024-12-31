98375270635be52491e9164bec770693edd377ed
Mukto Akash <muktoakash@gmail.com>
2024-12-30 21:31:13 -0500

    Set post route for RESTDemo

--- Node/Express/RESTDemo/index.js
+++ Node/Express/RESTDemo/index.js
@@ -4,6 +4,10 @@ const app = express();
 app.get('/tacos', (req, res) => {
     res.send("GET /tacos response");
 })
+
+app.post('/tacos', (req, res) => {
+    res.send("POST /tacos response");
+})
 app.listen(3000, () = {
     console.log("ON PORT 3000!");
 })
